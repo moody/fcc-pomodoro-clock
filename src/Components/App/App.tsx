7a70a41f7309e75dcc5f79909b24f6c9c4ab3d93
moody <dethanyel@gmail.com>
2019-04-15 19:53:06 -0400

    organize + add scss support

---
+++ src/Components/App/App.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Pomodoro Clock</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
