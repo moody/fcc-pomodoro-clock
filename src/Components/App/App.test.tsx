2f87545c259630d47befc40af4f3e83e4c187408
moody <dethanyel@gmail.com>
2019-04-16 06:11:57 -0400

    added enzyme

--- src/Components/App/App.test.tsx
+++ src/Components/App/App.test.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
+Enzyme.configure({ adapter: new Adapter() });
+
+const setup = () => {
+  const props = {};
+  const enzymeWrapper = shallow(<App {...props} />);
+  return {
+    props,
+    enzymeWrapper
+  };
+};
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const { enzymeWrapper } = setup();
 });
