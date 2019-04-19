c59c354ff4ae59623616e0857334a5a624b99386
moody <dethanyel@gmail.com>
2019-04-18 22:06:14 -0400

    Updates

---
+++ precache-manifest.df1e963452c46c6efe5e1862da183b1c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "418dad87601f9c8abd0e5798c0dc1feb",
+    "url": "/fcc-pomodoro-clock/static/media/fa-solid-900.418dad87.woff2"
+  },
+  {
+    "revision": "6fc650e97150e4a08f6e",
+    "url": "/fcc-pomodoro-clock/static/css/main.ccfacb8d.chunk.css"
+  },
+  {
+    "revision": "861edab1e102bec14060",
+    "url": "/fcc-pomodoro-clock/static/js/runtime~main.4b438cc6.js"
+  },
+  {
+    "revision": "49279363201ed19a840796e05a74a91b",
+    "url": "/fcc-pomodoro-clock/static/media/fa-solid-900.49279363.svg"
+  },
+  {
+    "revision": "5e711255134f0a720ba1",
+    "url": "/fcc-pomodoro-clock/static/js/2.99253857.chunk.js"
+  },
+  {
+    "revision": "e6257a726a0cf6ec8c6fec22821c055f",
+    "url": "/fcc-pomodoro-clock/static/media/fa-regular-400.e6257a72.woff2"
+  },
+  {
+    "revision": "427d721b86fc9c68b2e85ad42b69238c",
+    "url": "/fcc-pomodoro-clock/static/media/fa-regular-400.427d721b.woff"
+  },
+  {
+    "revision": "dcce4b7fbd5e895561e18af4668265af",
+    "url": "/fcc-pomodoro-clock/static/media/fa-regular-400.dcce4b7f.eot"
+  },
+  {
+    "revision": "65b9977aa23185e8964b36eddbce7a20",
+    "url": "/fcc-pomodoro-clock/static/media/fa-regular-400.65b9977a.ttf"
+  },
+  {
+    "revision": "5e2f92123d241cabecf0b289b9b08d4a",
+    "url": "/fcc-pomodoro-clock/static/media/fa-brands-400.5e2f9212.woff2"
+  },
+  {
+    "revision": "6fc650e97150e4a08f6e",
+    "url": "/fcc-pomodoro-clock/static/js/main.fe1731d4.chunk.js"
+  },
+  {
+    "revision": "2ef8ba3410dcc71578a880e7064acd7a",
+    "url": "/fcc-pomodoro-clock/static/media/fa-brands-400.2ef8ba34.woff"
+  },
+  {
+    "revision": "a7140145ebaaf5fb14e40430af5d25c4",
+    "url": "/fcc-pomodoro-clock/static/media/fa-solid-900.a7140145.woff"
+  },
+  {
+    "revision": "98b6db59be947f563350d2284fc9ea36",
+    "url": "/fcc-pomodoro-clock/static/media/fa-brands-400.98b6db59.ttf"
+  },
+  {
+    "revision": "a7b95dbdd87e0c809570affaf366a434",
+    "url": "/fcc-pomodoro-clock/static/media/fa-brands-400.a7b95dbd.eot"
+  },
+  {
+    "revision": "5eb754ab7dbd2fee562360528db4c3c0",
+    "url": "/fcc-pomodoro-clock/static/media/fa-regular-400.5eb754ab.svg"
+  },
+  {
+    "revision": "46e7cec623d8bd790d9fdbc8de2d3ee7",
+    "url": "/fcc-pomodoro-clock/static/media/fa-solid-900.46e7cec6.eot"
+  },
+  {
+    "revision": "ff8d9f8adb0d09f11d4816a152672f53",
+    "url": "/fcc-pomodoro-clock/static/media/fa-solid-900.ff8d9f8a.ttf"
+  },
+  {
+    "revision": "5bf145531213545e03ff41cd27df7d2b",
+    "url": "/fcc-pomodoro-clock/static/media/fa-brands-400.5bf14553.svg"
+  },
+  {
+    "revision": "5e711255134f0a720ba1",
+    "url": "/fcc-pomodoro-clock/static/css/2.3cc0fe9f.chunk.css"
+  },
+  {
+    "revision": "b862f3cfe7ee98a440e6e00cce9a29d4",
+    "url": "/fcc-pomodoro-clock/index.html"
+  }
+];
